dc54916c4d86cdd221f6a0ac66223bd23b63a8ac
Volkan Akdere <akderevolkan2@gmail.com>
2021-06-03 23:05:46 +0300

    üye olma özelliği getirildi.

--- OtoHaber/OtoHaber.MvcWebUI/Controllers/AuthController.cs
+++ OtoHaber/OtoHaber.MvcWebUI/Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 ﻿using OtoHaber.DataAccess.Concrete;
 using OtoHaber.Entities.DTOs;
+using OtoHaber.MvcWebUI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,32 @@ namespace OtoHaber.MvcWebUI.Controllers
     public class AuthController : Controller
     {
         KullaniciDal kullaniciDal = new KullaniciDal();
+
+        [HttpGet]
+        public ActionResult UyeOl()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UyeOl(AuthUyeOlViewModel model)
+        {
+            var yeniKullanici = model.Kullanici;
+            yeniKullanici.RolId = 2;
+            kullaniciDal.Ekle(yeniKullanici);
+            var kullaniciRolAdi = kullaniciDal.GetirKullaniciRolByKullanici(yeniKullanici);
+            var authTicket = new FormsAuthenticationTicket(
+                1,
+                yeniKullanici.Eposta,
+                DateTime.Now,
+                DateTime.Now.AddDays(3),
+                false,
+                kullaniciRolAdi);
+
+            var encryptedTicket = FormsAuthentication.Encrypt(authTicket);
+            Response.Cookies.Add(new HttpCookie(FormsAuthentication.FormsCookieName, encryptedTicket));
+            return RedirectToAction("Index", "Home");
+        }
 
         [HttpGet]
         public ActionResult Login()
---
+++ OtoHaber/OtoHaber.MvcWebUI/Models/AuthUyeOlViewModel.cs
@@ -0,0 +1,13 @@
+﻿using OtoHaber.Entities.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OtoHaber.MvcWebUI.Models
+{
+    public class AuthUyeOlViewModel
+    {
+        public Kullanici Kullanici { get; set; }
+    }
+}
